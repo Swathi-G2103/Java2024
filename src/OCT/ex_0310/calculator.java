2fc84773b1d624c6ad7c7011a1645b69a601ff08
al3971 <swathishivrambotla@gmail.com>
2024-10-04 18:08:28 +0530

    Print of 10 table

---
+++ src/OCT/ex_0310/calculator.java
@@ -0,0 +1,11 @@
+package OCT.ex_0310;
+
+public class calculator {
+    public static void main(String[] args) {
+        System.out.println(10+3);
+        System.out.println(103*8);
+        System.out.println(98-3);
+        System.out.println(90%4);
+    }
+}
+
