1347517e4161109715d2b112a79682e945f0883b
al3971 <swathishivrambotla@gmail.com>
2024-10-14 23:31:14 +0530

    Print of 10 table

---
+++ src/OCT1410/lab006explicitcast.java
@@ -0,0 +1,10 @@
+package OCT1410;
+
+public class lab006explicitcast {
+    public static void main(String[] args) {
+        double price = 19.99;
+        int roundedPrice = (int) price; // Explicit cast from double to int
+        System.out.println("Explicit cast (double to int): " + roundedPrice);
+
+    }
+}
---
+++ src/Oct0910/lab001nextline.java
@@ -0,0 +1,13 @@
+package Oct0910;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.sql.SQLOutput;
+
+public class lab001nextline {
+    public static void main (String[] args)
+            //main short form
+    {
+        System.out.print("hello java");
+        System.out.print("hello world ");//sout enter
+    }
+}
